93eb123f44cc2f4723c5c5ec1951b6ea86d36c7e
Tran Ba Minh Huy <tranbaminhhuy@Huys-MacBook-Air.local>
2025-11-06 09:43:02 +0700

    Added Frame 45, Flow 2 with responsive buttons for later uses.

--- 01-client/app/Q1.tsx
+++ 01-client/app/Q1.tsx
@@ -10,6 +10,28 @@ import { useRouter } from "expo-router";
 export default function Q1() {
   const router = useRouter();
 
+  const items = [
+    { id: 1, label: 'Tai', image: require('../assets/images/tai.png') },
+    { id: 2, label: 'Tóc', image: require('../assets/images/toc.png') },
+    { id: 3, label: 'Mắt', image: require('../assets/images/mat.png') },
+    { id: 4, label: 'Mũi', image: require('../assets/images/mui.png') },
+    { id: 5, label: 'Môi', image: require('../assets/images/moi.png') },
+    { id: 6, label: 'Răng', image: require('../assets/images/rang.png') },
+    { id: 7, label: 'Miệng', image: require('../assets/images/mieng.png') },
+    { id: 8, label: 'Bàn tay', image: require('../assets/images/bantay.png') },
+    { id: 9, label: 'Ngón tay', image: require('../assets/images/ngontay.png') },
+    { id: 10, label: 'Vai', image: require('../assets/images/vai.png') },
+    { id: 11, label: 'Lưng', image: require('../assets/images/lung.png') },
+    { id: 12, label: 'Bụng', image: require('../assets/images/bung.png') }, 
+    { id: 13, label: 'Đầu gối', image: require('../assets/images/daugoi.png') },
+    { id: 14, label: 'Bàn chân', image: require('../assets/images/banchan.png') },
+  ];
+
+  const rows = [];
+  for (let i = 0; i < items.length; i += 2) {
+    rows.push(items.slice(i, i + 2))
+  }
+
   const [loaded, error] = useFonts({
     'Nunito-Light': require('../assets/fonts/Nunito/static/Nunito-Light.ttf'),
     'Nunito-SemiBold': require('../assets/fonts/Nunito/static/Nunito-SemiBold.ttf')
@@ -35,7 +57,7 @@ export default function Q1() {
           style = {{
             width: 360,
             height: 24,
-            marginTop: 30
+            marginTop: 10
           }}
           contentFit="cover"
         />
@@ -55,6 +77,34 @@ export default function Q1() {
         </Text>
 
       </View>
+
+        <View>
+          {
+            rows.map((row, rowIndex) => (
+              <View key={rowIndex} style={styles.row}>
+                {
+                  row.map((item) => (
+                    <View key={item.id} style={[styles.card, {flexDirection: "row"}]}>
+                      <Pressable
+                        style={styles.press}
+                        onPress={() => console.log(`Pressed ${item.label}`)}
+                        >
+                      
+                        <Image
+                          source={item.image}
+                          style={styles.image}
+                        />
+                        <Text style={styles.nunitoSemiBold}>{item.label}</Text>
+
+                      </Pressable>
+                    </View>
+                  ))
+                }
+              </View>
+            ))          
+          } 
+        </View>
+
       
       <View style= {{
           justifyContent: "center",
@@ -63,7 +113,7 @@ export default function Q1() {
           width: 340,
           height: 50,
           borderRadius: 20, 
-          marginBottom: 60
+          marginBottom: 30
         }}>
 
         <Pressable
@@ -89,24 +139,39 @@ const styles = StyleSheet.create({
   nunitoLight: {
     fontFamily: "Nunito-Light",
     color: "#555",
-    fontSize: 16
+    fontSize: 20
   },
   nunitoSemiBold: {
     fontFamily: "Nunito-SemiBold",
     color: "#555",
   },
+  row: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between', 
+    marginBottom: 20 
+  },
   card:{
-    width: 152,
-    height: 62,
+    width: 140,
+    height: 55,
     backgroundColor: "white",
-    borderRadius: 15,
+    borderRadius: 10,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
+    marginHorizontal: 15,
+    marginBottom: 3,
+    paddingHorizontal: 10,
   },
   image:{
-    width: 44,
-    height: 44,
-    marginBottom: 5
+    width: 35,
+    height: 35,
+    marginBottom: 5,
+    marginRight: 10
+  },
+  press: {
+    flexDirection: 'row', 
+    alignItems: "center",
+    justifyContent: "flex-start",
+    width: 162,
+    height: 60
   }
-
-});
+})
